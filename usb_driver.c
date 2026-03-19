8be80f75bd2444fa9bffd4001eae3a82cddd033c
Mamun Al Rashid <mamunalrashid17@gmail.com>
2026-03-19 06:32:55 +0000

    Interfaced Digitek pen drive 19/3

--- usb_driver.c
+++ usb_driver.c
@@ -4,18 +4,22 @@
 static struct usb_device *device;
 
 static struct usb_device_id usb_table[] = {
-  {USB_DEVICE(0x781, 0x5567)},
+  {USB_DEVICE(0x346d, 0x5678)},
   {}
-}
+};
 
-static int usb_probe(struct usb_interface *interface, const struct usb_device_id) {
+MODULE_DEVICE_TABLE(usb, usb_table);
+
+static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id) {
   struct usb_host_interface *iface_desc;
   struct usb_endpoint_descriptor *endpoint;
   
   iface_desc = interface->cur_altsetting;
   
+  device = interface_to_usbdev(interface);
+  
   pr_info("Pen drive i/f %d now probed: {%04X:%04X}\n",
-  iface_desc->desc.bInterfaceNumber, id->idVendor, id->idProduct);
+  iface_desc->desc.bInterfaceNumber, device->descriptor.idVendor, device->descriptor.idProduct);
   
   pr_info("Number of endpoints: %02X\n", iface_desc->desc.bNumEndpoints);
   
@@ -24,14 +28,31 @@ static int usb_probe(struct usb_interface *interface, const struct usb_device_id
   for (int i = 0; i < iface_desc->desc.bNumEndpoints; i++) {
     endpoint = &iface_desc->endpoint[i].desc;
     
-    pr_info("ED[%d]->bEndpointAddress: 0x%02X\n", i, endpoint->bEndpointAddress);
+    pr_info("Endpoint %d:\n", i);
     
-    pr_info("ED[%d]->bmAttributes: 0x%02X\n", i, endpoint->bmAttributes);
+    // Address
+    pr_info("  Address: 0x%02X\n", endpoint->bEndpointAddress);
     
-    pr_info("ED[%d]->wMaxPacketSize: 0x%02X\n", i, endpoint->wMaxPacketSize);
+    // Direction
+    if (usb_endpoint_dir_in(endpoint))
+        pr_info("  Direction: IN\n");
+    else
+        pr_info("  Direction: OUT\n");
+    
+    // Transfer type
+    if (usb_endpoint_xfer_bulk(endpoint))
+        pr_info("  Type: Bulk\n");
+    else if (usb_endpoint_xfer_int(endpoint))
+        pr_info("  Type: Interrupt\n");
+    else if (usb_endpoint_xfer_control(endpoint))
+        pr_info("  Type: Control\n");
+    else if (usb_endpoint_xfer_isoc(endpoint))
+        pr_info("  Type: Isochronous\n");
+    
+    // Packet size    
+    pr_info("  MaxPacketSize: %d\n",
+            le16_to_cpu(endpoint->wMaxPacketSize));
   }
-  
-  device = interface_to_usbdev(interface);
   
   return 0;
 }
@@ -63,6 +84,7 @@ static int __init usb_module_init(void) {
 }
 
 static void __exit usb_module_exit(void) {
+  usb_deregister(&my_usb_driver);
   pr_info("USB module exited\n");
 }
 
